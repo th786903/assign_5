fdcb13d9ab31cb9d9491c8005512b759cf00842c
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-10-16 18:03:12 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -94,8 +94,8 @@ for i in range(6):
     node.addService(pg.Execute(shell="sh", command="sudo firewall-cmd --permanent --zone public --add-service nfs"))
     node.addService(pg.Execute(shell="sh", command="sudo firewall-cmd --reload"))
     #mount
-    node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.3:/scratch /scratch"))
-    node.addService(pg.Execute(shell="sh", command="sudo echo '192.168.1.3:/scratch /scratch nfs rw,sync,hard,intr 0 0' >> etc/fstab"))
+    #node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.3:/scratch /scratch"))
+    #node.addService(pg.Execute(shell="sh", command="sudo echo '192.168.1.3:/scratch /scratch nfs rw,sync,hard,intr 0 0' >> etc/fstab"))
     #install mpi ----------- just testing mounting
     node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
@@ -133,10 +133,11 @@ for i in range(6):
     node.addService(pg.Execute(shell="sh", command="sudo mkdir /software"))
     node.addService(pg.Execute(shell="sh", command="sudo mkdir /scratch"))
     
-    node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.1:/software /software"))
-    node.addService(pg.Execute(shell="sh", command="sudo echo '192.168.1.1:/software /software nfs rw,sync,hard,intr 0 0' >> etc/fstab"))
-    node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.3:/scratch /scratch"))
-    node.addService(pg.Execute(shell="sh", command="sudo echo '192.168.1.3:/scratch /scratch nfs rw,sync,hard,intr 0 0' >> etc/fstab"))
+    #mount
+    #node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.1:/software /software"))
+    #node.addService(pg.Execute(shell="sh", command="sudo echo '192.168.1.1:/software /software nfs rw,sync,hard,intr 0 0' >> etc/fstab"))
+    #node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.3:/scratch /scratch"))
+    #node.addService(pg.Execute(shell="sh", command="sudo echo '192.168.1.3:/scratch /scratch nfs rw,sync,hard,intr 0 0' >> etc/fstab"))
     
 
 
