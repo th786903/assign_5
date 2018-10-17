9fbbd4261b4068a4672bee55836eb7c222b4abe2
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-10-17 06:51:26 -0400

    Update compute_setup.sh

--- compute_setup.sh
+++ compute_setup.sh
@@ -2,14 +2,14 @@
 sudo yum install nfs-utils
 
 # make nfs directory mount points
-sudo mkdir -p /mnt/nfs/var/scratch
-sudo mkdir -p /mnt/nfs/var/software
+sudo mkdir -p /scratch
+sudo mkdir -p /software
 
 # mount nfs shared directory to the mount point
-sudo mount -t nfs 192.168.1.3:/var/scratch /mnt/nfs/var/scratch
-sudo mount -t nfs 192.168.1.1:/var/software /mnt/nfs/var/software
+sudo mount -t nfs 192.168.1.3:/scratch /scratch
+sudo mount -t nfs 192.168.1.1:/software /software
 
 #i think i have to echo into something like /mnt/nfs/var blah
 # automount
-sudo echo "192.168.1.3:/var/scratch /mnt/nfs/var/scratch nfs defaults 0 0" >> /etc/fstab
-sudo echo "192.168.1.1:/var/software /mnt/nfs/var/software nfs defaults 0 0" >> /etc/fstab
+sudo echo "192.168.1.3:/scratch /scratch nfs defaults 0 0" >> /etc/fstab
+sudo echo "192.168.1.1:/software /software nfs defaults 0 0" >> /etc/fstab
