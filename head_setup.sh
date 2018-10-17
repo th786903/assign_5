77272f5ed59dbc5aa74ad35996e8855379d72b86
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-10-17 06:53:14 -0400

    Update head_setup.sh

--- head_setup.sh
+++ head_setup.sh
@@ -2,10 +2,10 @@
 sudo yum install nfs-utils
 
 # making software directory
-sudo mkdir /var/software
+sudo mkdir /software
 
 # setting permissions
-sudo chmod -R 777 /var/software
+sudo chmod -R 777 /software
 
 # setting up nfs
 sudo systemctl enable rpcbind
@@ -18,7 +18,7 @@ sudo systemctl start nfs-lock
 sudo systemctl start nfs-idmap
 
 # setup export directory
-sudo echo "/var/software *(rw,sync,no_root_squash,no_all_squash)" > etc/exports
+sudo echo "/software *(rw,sync,no_root_squash,no_all_squash)" > etc/exports
 sudo systemctl restart nfs-server
 
 # let nfs service override centos7 firewall
@@ -28,10 +28,10 @@ sudo firewall-cmd --permanent --zone=public --add-service=rpc-bind
 sudo firewall-cmd --reload
 
 # set client up, setup mount point from storage, to head
-sudo mkdir -p /mnt/nfs/var/scratch
-sudo mount -t nfs 192.168.1.3:/var/scratch /mnt/nfs/var/scratch
+sudo mkdir -p /scratch
+sudo mount -t nfs 192.168.1.3:/scratch /scratch
 
 # setup automount
-sudo echo "192.168.1.3:/var/scratch /mnt/nfs/var/scratch nfs defaults 0 0" >> /etc/fstab
+sudo echo "192.168.1.3:/scratch /scratch nfs defaults 0 0" >> /etc/fstab
 
 
