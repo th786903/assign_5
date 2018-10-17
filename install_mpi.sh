fa7a06abc3f79cb97233915200ddf44cc8d9c1ce
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-10-17 06:54:14 -0400

    Update install_mpi.sh

--- install_mpi.sh
+++ install_mpi.sh
@@ -4,11 +4,11 @@ sudo yum -y group install "Development Tools"
 sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
 sudo tar xzf openmpi-3.1.2.tar.gz
 cd openmpi-3.1.2
-sudo ./configure --prefix=/var/software/openmpi/3.1.2
+sudo ./configure --prefix=/software/openmpi/3.1.2
 sudo make
 sudo make all install
-echo "export PATH='$PATH:/var/software/openmpi/3.1.2/bin'" >> /users/ka837933/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/var/software/openmpi/3.1.2/lib/'" >> /users/ka837933/.bashrc
+echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/ka837933/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/ka837933/.bashrc
 cd ..
 sudo rm -Rf openmpi-3.1.2
 sudo rm -Rf openmpi-3.1.2.tar.gz
