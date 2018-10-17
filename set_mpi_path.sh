478b25b147a4ec469d59baac0f2fda92cf2beedc
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-10-17 03:31:07 -0400

    Create set_mpi_path.sh

---
+++ set_mpi_path.sh
@@ -0,0 +1,2 @@
+echo "export PATH='$PATH:/mnt/nfs/var/software/openmpi/3.1.2/bin'" >> /users/ka837933/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/mnt/nfs/var/software/openmpi/3.1.2/lib/'" >> /users/ka837933/.bashrc
