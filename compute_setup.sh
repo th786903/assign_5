88e28ef52ec62c7ebfe4bd043f49a52cae5c6794
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-10-16 23:31:20 -0400

    Update compute_setup.sh

--- compute_setup.sh
+++ compute_setup.sh
@@ -9,6 +9,7 @@ sudo mkdir -p /mnt/nfs/var/software
 sudo mount -t nfs 192.168.1.3:/var/scratch /mnt/nfs/var/scratch
 sudo mount -t nfs 192.168.1.1:/var/software /mnt/nfs/var/software
 
+#i think i have to echo into something like /mnt/nfs/var blah
 # automount
-sudo echo '192.168.1.3:/var/scratch /mnt/nfs/var/scratch nfs defaults 0 0' >> etc/fstab
-sudo echo '192.168.1.1:/var/software /mnt/nfs/var/software nfs defaults 0 0' >> etc/fstab
+sudo echo '192.168.1.3:/var/scratch /mnt/nfs/var/scratch nfs defaults 0 0' >> /etc/fstab
+sudo echo '192.168.1.1:/var/software /mnt/nfs/var/software nfs defaults 0 0' >> /etc/fstab
