e7587a9419418c9d73f95484ab5dede5961df036
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-10-18 18:02:02 -0400

    Update set_mpi_path.sh

--- set_mpi_path.sh
+++ set_mpi_path.sh
@@ -1,2 +1,3 @@
 echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/ka837933/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/ka837933/.bashrc
+source /users/ka837933/.bashrc
