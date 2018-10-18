8ef847830156753a165a51e39363d6654ebadd55
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-10-18 18:58:40 -0400

    Update head_setup.sh

--- head_setup.sh
+++ head_setup.sh
@@ -28,8 +28,13 @@ sudo firewall-cmd --permanent --zone=public --add-service=rpc-bind
 sudo firewall-cmd --reload
 
 # set client up, setup mount point from storage, to head
+sudo sleep 180
 sudo mkdir -p /scratch
 sudo mount -t nfs 192.168.1.3:/scratch /scratch
 # setup automount
 sudo echo "192.168.1.3:/scratch /scratch nfs defaults 0 0" >> /etc/fstab
+
+#install mpi
+sudo chmod 777 /local/repository/install_mpi.sh
+sudo /local/repository/install_mpi.sh
 
