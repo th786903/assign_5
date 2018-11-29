be2b86cb6c6ffa583a58cd8b483200f8c6c19c82
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-11-29 12:20:58 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -77,8 +77,8 @@ for i in range(5):
   node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))
   
   # This code segment is added per Benjamin Walker's solution to address the StrictHostKeyCheck issue of ssh
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo -H -u ka837933 bash -c '/local/repository/ssh_setup.sh'"))
+  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup_Two.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo -H -u ka837933 bash -c '/local/repository/ssh_setup_Two.sh'"))
  
   node.addService(pg.Execute(shell="sh", command="sudo su ka837933 -c 'cp /local/repository/source/* /users/ka837933'"))
   
