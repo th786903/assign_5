d389248f74c44b089566c9994a0634cc8aa3d748
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-12-01 20:22:56 -0500

    Create metadata_setup.sh

---
+++ metadata_setup.sh
@@ -0,0 +1,17 @@
+#making it sleep so head and storage are set up
+sudo sleep 30m
+# installing nfs
+sudo yum install nfs-utils
+
+# make nfs directory mount points
+sudo mkdir -p /scratch
+sudo mkdir -p /software
+
+# mount nfs shared directory to the mount point
+sudo mount -t nfs 192.168.1.3:/scratch /scratch
+sudo mount -t nfs 192.168.1.1:/software /software
+
+#i think i have to echo into something like /mnt/nfs/var blah
+# automount
+sudo echo "192.168.1.3:/scratch /scratch nfs defaults 0 0" >> /etc/fstab
+sudo echo "192.168.1.1:/software /software nfs defaults 0 0" >> /etc/fstab
