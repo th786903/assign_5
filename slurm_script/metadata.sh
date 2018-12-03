f481042413f11dd1f8e976ce50a3302570b35f7e
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-12-03 01:44:32 -0500

    Update metadata.sh

--- slurm_script/metadata.sh
+++ slurm_script/metadata.sh
@@ -47,7 +47,7 @@ done
 sudo yum --nogpgcheck localinstall /software/slurm-rpms/* -y
 
 # copying slurm.conf file
-sudo cp /local/repository/source/slurm.conf /scratch
+sudo cp /local/repository/source/slurm.conf /etc/slurm
 
 # setting up configurations and files
 sudo mkdir /var/spool/slurmd
