21c3ebe36ca3d7ed09203b8073000c7e982e0c45
RedHorizons <45643454+RedHorizons@users.noreply.github.com>
2018-12-09 20:33:28 -0500

    Update metadata.sh

--- slurm_script/metadata.sh
+++ slurm_script/metadata.sh
@@ -58,7 +58,9 @@ sudo chmod 755 /var/log/slurm/slurmdbd.log
 sudo touch /var/run/slurmdbd.pid
 sudo chown slurm: /var/run/slurmdbd.pid
 sudo chmod 777 /var/run/slurmdbd.pid
-
+sudo cp /local/repository/source/innodb.cnf /etc/my.cnf.d/innodb.cnf
+sudo chown slurm: /etc/my.cnf.d/innodb.cnf
+sudo chmod 777 /etc/my.cnf.d/innodb.cnf
 
 
 # disabling firewall
